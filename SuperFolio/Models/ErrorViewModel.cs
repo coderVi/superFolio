9fb72dfc497219891142a768d6b983fcad8d2d7a
coderVi <mr.asmtylmz@gmail.com>
2024-07-19 17:20:47 +0300

    Project Created

---
+++ SuperFolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuperFolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
